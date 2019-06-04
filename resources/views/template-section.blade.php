71408022b3c62f9973410b64fd1b45b454983d7b
Jeric Epon <jeric.epon@stokmanila.com>
2019-06-04 12:28:48 +0800

    newsletter mobile

--- resources/views/template-section.blade.php
+++ resources/views/template-section.blade.php
@@ -14,22 +14,24 @@
       <div class="newsletter row px-0">
         <div class="col-lg-6 col-md-6 col-sm-12  newsletter-cover" style="background-image: url(@asset('images/temp/newsletter.png')">
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 py-4  d-flex justify-content-center align-items-center newsletter-content">
+        <div class="col-lg-6 col-md-6 col-sm-12 p-4  d-flex justify-content-center align-items-center newsletter-content">
           <div class="newsletter-inner">
             <div class="newsletter-title">Håll dig uppdaterad</div>
             <div class="newsletter-body">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam facilis consequuntur reiciendis repellendus minus quae non soluta quisquam aut tempore ex odit accusantium molestiae ab, officia deserunt ad numquam, laudantium nemo nostrum corporis. Quae reprehenderit, enim id porro similique atque illum non corporis, quod distinctio aperiam velit asperiores quasi sint pariatur error cumque omnis iusto perferendis rem laboriosam est, et impedit. Provident fuga qui vero inventore error cumque quo. Eligendi enim quae explicabo repudiandae illo maiores voluptate maxime animi, nisi, natus atque perspiciatis aliquid rerum quod corrupti aliquam officiis dolor debitis tenetur? Pariatur corrupti non debitis hic, sit quisquam magni.
             </div>
             <div class="row m-0 align-items-center justify-content-center justify-content-lg-around newsletter-form">
-              <div class="form-group mb-0">
-                <input type="text" class="form-control form-control-lg">
-                <div class="invalid-feedback">
-                  Your email is not valid
-                </div>
+              <div class="input-group is-invalid">
+                <input type="text" class="form-control is-invalid" name="name" id="name" placeholder="" aria-label="">
+                <span class="input-group-btn">
+                  <button class="btn btn-lg btn-icon btn-icon-lg btn-outline-primary shadow-sm border-0" type="button">
+                    <img src="@asset('images/icon/arrow-right.svg')" alt="" srcset="">
+                  </button>
+                </span>
               </div>
-              <button class="btn btn-lg btn-icon btn-icon-lg btn-outline-primary shadow-sm border-0" type="button">
-                <img src="@asset('images/icon/arrow-right.svg')" alt="" srcset="">
-              </button>
+              <div class="invalid-feedback text-left">
+                Validation message
+                </div>
             </div>
 
             <div class="mt-2 custom-control custom-checkbox">
