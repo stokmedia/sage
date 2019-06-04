08a4794e71d66c3a935f81b84c1c9ed689091785
vannymagalong <vannessa@stokmanila.com>
2019-06-04 21:25:27 +0800

    USP update

--- resources/views/partials/sections.blade.php
+++ resources/views/partials/sections.blade.php
@@ -1,3 +1,6 @@
-@while(have_rows( 'sections' )) @php the_row(); @endphp
-  @include('sections.section-'.get_row_layout())
-@endwhile
+@if (!empty($sections))
+    @foreach($sections as $section)
+        @include( 'sections.section-'.$section->acf_fc_layout )
+    @endforeach
+@endif
+
--- resources/views/sections/section-usp.blade.php
+++ resources/views/sections/section-usp.blade.php
@@ -1,37 +1,29 @@
-{{-- TODO: This is just copied from SysterP as a test
-Should be rewritten to clean Blade code --}}
-
-@if (get_sub_field('usp'))
-	@php 
-		$list = get_sub_field('usp')
-	@endphp
+@if ($section->usp)
+	@php ($list = $section->usp)
 @else
-	@php
-		$usp = get_field('default_usp', App::currentLang() );
-		$list = $usp['usp'];	
-	@endphp
+	@php ($list = $default_usp)
 @endif
 
-@if ($list)
+@if (!empty($list))
 	<div class="trust-bar">
 		<div class="container">
 			
 			@foreach ($list as $item)
 				<div class="trust-item">
 
-					@if ($item['image'])
+					@if ($item->image)
 						<div class="icon-block">
-							<img width="40" height="40" src="{{ wp_get_attachment_image_url( $item['image'] ) }}" class="" alt=""/>
+							<img width="40" height="40" src="{{ wp_get_attachment_image_url( $item->image ) }}" class="" alt=""/>
 						</div>
 					@endif
 
 					<div class="info">
-						@if ($item['title'])
-							<h4 class="h4">{{ $item['title'] }}</h4>
+						@if ($item->title)
+							<h4 class="h4">{{ $item->title }}</h4>
 						@endif
 
-						@if ($item['text'])
-							<div class="small">{{ $item['text'] }}</div>
+						@if ($item->text)
+							<div class="small">{{ $item->text }}</div>
 						@endif
 					</div>
 				</div>
