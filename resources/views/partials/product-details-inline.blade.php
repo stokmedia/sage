af9e78726a56e11ea22fa6ded4f87c4b46218584
janstok <illadash3020@gmail.com>
2019-06-06 20:36:35 +0800

    thank you section

---
+++ resources/views/partials/product-details-inline.blade.php
@@ -0,0 +1,17 @@
+<div class="product-details-inline {{$classname}}">
+  <div class="image-wrap">
+    <div class="image">
+      <figure>
+        <img src="@asset('images/temp/erika-skirt1.jpeg')" alt="">
+      </figure>
+    </div>
+  </div>
+  <div class="info">
+    <div class="name">ERIKA SKIRT</div>
+    <div class="category">Kategori - Kjolar</div>
+    <div class="color">Färg - OCEAN BLUE</div>
+    <div class="size">Storlek - M</div>
+    <div class="price">Pris - 799 kr</div>
+    <div class="price is-sale">Pris - <span class="old-price">799 kr</span> <span class="new-price">799 kr</span></div>
+  </div>
+</div>
---
+++ resources/views/sections/section-thankyou.blade.php
@@ -0,0 +1,34 @@
+<section class="section thankyou-details">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5 offset-lg-1 col-md-6">
+        <h3 class="h3">Produkter</h3>
+        @include('partials.product-details-inline', ['classname'=>'is-large'])
+      </div>
+      <div class="col-lg-6 col-md-6">
+        <div class="summary">
+          <h3 class="h3">Summering</h3>
+          <div class="summary-group">
+            <div class="summary-item">
+              <div class="title">Totalt</div>
+              <div class="price">799 kr</div>
+            </div>
+            <div class="summary-item">
+              <div class="title">Moms</div>
+              <div class="price">46 kr</div>
+            </div>
+            <div class="summary-item">
+              <div class="title">Frakt</div>
+              <div class="price">0 kr</div>
+            </div>
+            <div class="summary-item">
+              <div class="title">Sub Totalt</div>
+              <div class="price"><strong>799  kr</strong></div>
+            </div>
+          </div>
+        </div>
+        <div class="summary-small small">In hac habitasse platea dictumst. Vivamus adipiscing fermentum quam volu tpat aliquam. Integer et elit eget elit facilisis tristique. Nam vel iaculis mauris. Sed ullamcorper tellus erat, non ultrices sem tincidunt euismod. Fusce rhoncus porttitor velit, eu bibendum nibh aliquet vel. Fusce lorem leo, vehicula</div>
+      </div>
+    </div>
+  </div>
+</section>
