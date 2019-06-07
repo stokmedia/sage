407dff71c433082508265664aba60c68f8c828a6
EJ Perez <idyey04@gmail.com>
2019-06-07 17:16:52 +0800

    Make desktop menu dynamic

--- app/Controllers/App.php
+++ app/Controllers/App.php
@@ -10,36 +10,56 @@ class App extends Controller
     use Partials\Content;
 
     protected $acf = true;
-    
+
     public function siteName()
     {
-        return get_bloginfo('name');
+        return get_bloginfo( 'name' );
     }
 
     public static function title()
     {
-        if (is_home()) {
-            if ($home = get_option('page_for_posts', true)) {
-                return get_the_title($home);
+        if ( is_home() ) {
+            if ( $home = get_option( 'page_for_posts', true ) ) {
+                return get_the_title( $home );
             }
-            return __('Latest Posts', 'sage');
+            return __( 'Latest Posts', 'sage' );
         }
-        if (is_archive()) {
+        if ( is_archive() ) {
             return get_the_archive_title();
         }
-        if (is_search()) {
-            return sprintf(__('Search Results for %s', 'sage'), get_search_query());
+        if ( is_search() ) {
+            return sprintf( __( 'Search Results for %s', 'sage' ), get_search_query() );
         }
-        if (is_404()) {
-            return __('Not Found', 'sage');
+        if ( is_404() ) {
+            return __( 'Not Found', 'sage' );
         }
         return get_the_title();
     }
-/*
-    // TODO: Could we use a general function to generate the image tag in the templates for Centra Product Images
-    public function renderProductImage( $imageData, $size ) {
 
+    public static function desktopNavigation()
+    {
+        if ( !has_nav_menu( 'header_navigation' ) ) {
+            return null;
+
+        }
+
+        // Add or modify classes
+        $menu = wp_nav_menu( [
+            'theme_location' => 'header_navigation',
+            'depth' => 1,
+            'echo' => false,
+            'menu_class' => 'navbar-nav m-auto' ] );
+
+        $menu = str_replace( 'menu-item', 'nav-item menu-item', $menu );
+        $menu = str_replace( 'href', 'class="nav-link" href', $menu );
+
+        return $menu;
     }
-*/
+    /*
+        // TODO: Could we use a general function to generate the image tag in the templates for Centra Product Images
+        public function renderProductImage( $imageData, $size ) {
+
+        }
+    */
 
 }
