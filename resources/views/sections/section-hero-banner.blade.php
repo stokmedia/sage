2b9e57061c0cb90b4e5d03d01c2c32fb5ff4fd23
vannymagalong <vannessa@stokmanila.com>
2019-06-10 20:57:45 +0800

    Display static hero banner markup for now (for testing)

--- resources/views/sections/section-hero-banner.blade.php
+++ resources/views/sections/section-hero-banner.blade.php
@@ -1,12 +1,9 @@
-<section class='section-hero-banner'>
+{{-- <section class='section-hero-banner'>
     <div class='hero-wrap'>
         
         <div class='hero-background'>
-            {{-- Image Tag --}}
-            {{-- <img class='hero-image' src="@asset('images/temp/hero_banner.png')" alt=""> --}}
             {!! $section->image !!}            
 
-            {{-- Play button --}}
             @if ($section->video_url)
                 <div class='hero-play-btn js-hide-on-play js-playvideo'>
                     <button id='js-playvideo' class='btn btn-lg btn-icon btn-icon-lg btn-primary'>
@@ -17,16 +14,6 @@
         </div>
 
         {!! $section->video !!}
-
-        <!-- Video Tag -->
-        <!-- <video class='hero-video js-video-tag' autoplay='autoplay' muted='muted' loop='loop' playsinline='playsinline'>
-            <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
-        </video> -->
-        <!-- Iframe Tag -->
-        <!-- Vimeo -->
-        <!-- <iframe data-autoplayMobile='1' data-autoplay='1' class='hero-iframe-video is-vimeo js-video-iframe is-video-hidden' style='z-index:1;opacity:0.000001;background-color:black'; data-source='vimeo' src="https://player.vimeo.com/external/323460867.hd.mp4?s=6386bff00f0b9898e3fb3b84182057fdbcd3117d&profile_id=175" frameborder="0" allow="autoplay; fullscreen"></iframe> -->
-        <!-- Youtube -->
-        <!-- <iframe id="video-iframe-ScMzIvxBSi4" data-autoplayMobile='1' data-autoplay='' class='hero-iframe-video is-yt js-video-iframe' style='z-index:10;opacity:0.000001;background-color:black'; data-source='youtube' src="https://www.youtube.com/embed/ScMzIvxBSi4?rel=0&showinfo=0&controls=0&enablejsapi=1" frameborder="0" allow="autoplay; fullscreen"></iframe> -->
         
         @if ($section->title || $section->text || $section->link)
             <div class='hero-text'>
@@ -50,6 +37,56 @@
                 @endif
             </div>
         @endif
+
+    </div>
+</section> --}}
+
+
+{{-- Original Markup --}}
+
+<section class='section-hero-banner'>
+    <div class='hero-wrap'>
+        
+        <div class='hero-background'>
+            <!-- Image Tag -->
+            <img class='hero-image' src="@asset('images/temp/hero_banner.png')" alt="">
+
+            <!-- Play button -->
+            <!-- <div class='hero-play-btn js-hide-on-play js-playvideo'>
+                <button id='js-playvideo' class='btn btn-lg btn-icon btn-icon-lg btn-primary'>
+                    <img src="@asset('images/icon/icon-play.svg')" alt="">
+                </button>
+            </div> -->
+        </div>
+
+        <!-- Video Tag -->
+        <!-- <video class='hero-video js-video-tag' autoplay='autoplay' muted='muted' loop='loop' playsinline='playsinline'>
+            <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+        </video> -->
+        <!-- Iframe Tag -->
+        <!-- Vimeo -->
+        <!-- <iframe data-autoplayMobile='1' data-autoplay='1' class='hero-iframe-video is-vimeo js-video-iframe is-video-hidden' style='z-index:1;opacity:0.000001;background-color:black'; data-source='vimeo' src="https://player.vimeo.com/external/323460867.hd.mp4?s=6386bff00f0b9898e3fb3b84182057fdbcd3117d&profile_id=175" frameborder="0" allow="autoplay; fullscreen"></iframe> -->
+        <!-- Youtube -->
+        <!-- <iframe id="video-iframe-ScMzIvxBSi4" data-autoplayMobile='1' data-autoplay='' class='hero-iframe-video is-yt js-video-iframe' style='z-index:10;opacity:0.000001;background-color:black'; data-source='youtube' src="https://www.youtube.com/embed/ScMzIvxBSi4?rel=0&showinfo=0&controls=0&enablejsapi=1" frameborder="0" allow="autoplay; fullscreen"></iframe> -->
+        
+
+        <div class='hero-text'>
+            <h1 class='h1 hero-title'>Hero banner</h1>
+            <div class='hero-subtitle'>
+                För en aktiv livsstil
+            </div>
+            <div class='hero-btn'>
+        
+                <a href="#">
+                    <button class="btn btn-outline-primary d-md-none" type="button">Visa produkter</button>
+                </a>
+
+            
+                <a href="#">
+                    <button class="btn btn-lg btn-outline-primary d-none d-md-inline-block" type="button">Visa produkter</button>
+                </a>
+            </div>
+        </div>
 
     </div>
 </section>
