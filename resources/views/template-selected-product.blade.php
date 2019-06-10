5ae50b1f78934433cda97ce723ed6c407d2c9be4
Jeric Epon <jeric.epon@stokmanila.com>
2019-06-10 14:10:59 +0800

    Selected product (WIP)

--- resources/views/template-selected-product.blade.php
+++ resources/views/template-selected-product.blade.php
@@ -12,12 +12,20 @@
   <div class="container">
     <div class="row">
       <div class="selected-product-preview col-md-6 bg-lightgreen">
-        <div class="selected-product-slider">
-          <img src="@asset('images/temp/selected-product-view.png')" alt="" srcset="">
+        <div class="row mx-0">
+          <div class="col-md-11 px-0 selected-product-slider">
+            <img src="@asset('images/temp/selected-product-view.png')" alt="" srcset="">
+          </div>
+          <div class="col-md-1 px-0 selected-product-thumbnail">
+            @for ($thumbnail = 0; $thumbnail < 3; $thumbnail++)
+              <div class="item">
+                <img src="@asset('images/temp/selected-product-thumb.png')" alt="" srcset="">
+              </div>
+            @endfor
+          </div>
         </div>
       </div>
       <div class="selected-product-options col-md-6 bg-lightgreen">
-        <img src="@asset('images/temp/selected-product-view.png')" alt="" srcset="">
       </div>
     </div>
   </div>
