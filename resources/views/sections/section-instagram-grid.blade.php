f5958752a716040c09be577e6c28c9caeffa5995
janstok <illadash3020@gmail.com>
2019-06-10 21:54:36 +0800

    instagram desktop grid layout

--- resources/views/sections/section-instagram-grid.blade.php
+++ resources/views/sections/section-instagram-grid.blade.php
@@ -1,1 +1,53 @@
-<h2>Instagram</h2>
+<section class="section">
+    <div class="instagram-gallery">
+      <div class="col-wrap instagram-info-margin">
+        <div class="col-group">
+          <div class="col-side"></div>
+          <div class="col-side">
+            <div class="instagram-info">
+                <div class="btn-header d-flex justify-content-center">
+                  <a href="#" class="btn btn-sm btn-primary text-uppercase">FOLLOW US</a>
+                  <h3 class="h3">@skhoopskirts</h3>
+                </div>
+                <div class="info">In hac habitasse platea dictumst. Vivamus adipiscing fermentum quam volutpat aliquam. Integer et elit eget elit facilisis tristique. Nam vel iaculis mauris. Sed ullamcorper tellus erat, non ultrices sem tincidunt euismod. Fusce </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-wrap">
+        <div class="col-group">
+          <div class="col-side">
+            <div class="tile-group justify-content-end align-items-end">
+              <div class="tile is-three"></div>
+              <div class="tile-block">
+                <div class="tile"></div>
+                <div class="tile"></div>
+              </div>
+            </div>
+            <div class="tile-group justify-content-end">
+              <div class="tile"></div>
+              <div class="tile"></div>
+              <div class="tile"></div>
+            </div>
+          </div>
+          <div class="col-side">
+            <div class="tile-group">
+              <div class="tile-block-hidden"></div>
+            </div>
+            <div class="tile-group">
+              <div class="tile"></div>
+              <div class="tile"></div>
+              <div class="tile"></div>
+            </div>
+            <div class="tile-group">
+              <div class="tile-block">
+                <div class="tile"></div>
+                <div class="tile"></div>
+              </div>
+              <div class="tile is-three"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
