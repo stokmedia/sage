372282c165d5e5f5f9ce0a7191a723133ba3d9c5
Marc Anniel Vino <marc.vino@stokmanila.com>
2019-06-10 20:55:35 +0800

    Cookie Emoticon

--- resources/views/partials/cookies.blade.php
+++ resources/views/partials/cookies.blade.php
@@ -2,10 +2,10 @@
   <div class="alert alert-ash alert-dismissible fade show" role="alert">
     <div class="alert-container align-items-center d-flex justify-content-center flex-column flex-sm-row">
       <div class="col d-flex align-items-center p-0 flex-column flex-sm-row">
-          <img src="https://img.icons8.com/ios/50/000000/sad.png" alt="Alert">
+          <img src="@asset('images/icon/bored.svg')" alt="Alert">
           <span class="alert-text">Cookies - We use cookies. Read more.</span>
       </div>
-      <button class="btn btn-sm btn-primary text-body js-cookie-approve" type="button">Call to action</button>
+      <button class="btn btn-sm btn-primary text-body js-cookie-approve" type="button">Im fine with that</button>
     </div>
   </div>
 </div>
