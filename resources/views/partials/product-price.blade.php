2c9c4327b60c1e55a8ce4e7a175ad3613b13d976
vannymagalong <vannessa@stokmanila.com>
2019-06-11 17:01:24 +0800

    Make popular product section dynamic

--- resources/views/partials/product-price.blade.php
+++ resources/views/partials/product-price.blade.php
@@ -1,18 +1,17 @@
-{{-- TODO: This is copied from SysterP, Replace syntax below with Blade syntax --}}
+<div class="product-price">
+	@if ($priceInfo['success'])
+		@php
+		$priceAsNumber = !empty( $priceInfo['info']['price'] ) ? $priceInfo['info']['price'] : '';
+		$priceBeforeAsNumber = !empty( $priceInfo['info']['priceBeforeDiscount'] ) ? $priceInfo['info']['priceBeforeDiscount'] : '';	
+		@endphp
 
-@php
-if( $priceInfo['success'] ) {
-	$priceAsNumber = !empty( $priceInfo['info']['price'] ) ? $priceInfo['info']['price'] : '';
-	$priceBeforeAsNumber = !empty( $priceInfo['info']['priceBeforeDiscount'] ) ? $priceInfo['info']['priceBeforeDiscount'] : '';
-
-	if( $priceInfo['info']['showAsOnSale'] || ($priceBeforeAsNumber && $priceBeforeAsNumber > $priceAsNumber ) ) { // On Sale
-		echo '<strong class="price is-sale">'. $priceAsNumber .'</strong>';
-		echo '<span class="old-price">'. $priceBeforeAsNumber.'</span>'; // Original Price
-	} else {
-		echo '<span class="price">'. $priceAsNumber .'</span>';
-	}
-
-} else {
-		echo '<span class="price not-available">N/A</span>';
-}
-@endphp
+		@if ($priceInfo['info']['showAsOnSale'] || ($priceBeforeAsNumber && $priceBeforeAsNumber > $priceAsNumber ))
+			<span>{{ $priceBeforeAsNumber }}</span>
+			{{ $priceAsNumber }}
+		@else
+			<span>{{ $priceAsNumber }}</span>
+		@endif
+	@else
+		<span>N/A</span>
+	@endif
+</div>
