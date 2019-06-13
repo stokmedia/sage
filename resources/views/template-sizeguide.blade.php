79500ccf8bf1667eff78786100ecdc0ceddadacb
Marc Anniel Vino <marc.vino@stokmanila.com>
2019-06-13 21:44:22 +0800

    Size Guide HTML

---
+++ resources/views/template-sizeguide.blade.php
@@ -0,0 +1,189 @@
+{{--
+  Template Name: Size Guide Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+
+  <section class="section text-button has-pt">
+    <div class="container">
+        <h1 class="title h2">Storleksguide</h1>
+        <div class="preamble">Så här mäter du:
+          (det är dina egna mått du mäter som du sedan jämför i tabellen nedan
+          och ser vilken storlek du ska ha )
+        </div>
+        <div class="size-guide d-lg-flex">
+          <div class="size-guide-image">
+            <img src="@asset('images/temp/size-guide.png')" />
+          </div>
+          <div class="size-guide-table">
+            <div class="d-table">
+              <div class="d-table-row">
+                  <div class="d-table-cell text-center head"></div>
+                  <div class="d-table-cell text-center head">Bystvidd</div>
+                  <div class="d-table-cell text-center head">Midjevidd</div>
+                  <div class="d-table-cell text-center head">Stussvidd</div>
+              </div>
+              <div class="d-table-row">
+                <div class="d-table-cell size text-right text-right">
+                  XS
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+              </div>
+              <div class="d-table-row spacer"></div>
+              <div class="d-table-row">
+                <div class="d-table-cell size text-right">
+                  S
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+              </div>
+              <div class="d-table-row spacer"></div>
+              <div class="d-table-row">
+                <div class="d-table-cell size text-right">
+                  M
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+              </div>
+              <div class="d-table-row spacer"></div>
+              <div class="d-table-row">
+                <div class="d-table-cell size text-right">
+                  LG
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+              </div>
+              <div class="d-table-row spacer"></div>
+              <div class="d-table-row">
+                <div class="d-table-cell size text-right">
+                  XL
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+              </div>
+              <div class="d-table-row spacer"></div>
+              <div class="d-table-row">
+                <div class="d-table-cell size text-right">
+                  XXL
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+              </div>
+              <div class="d-table-row spacer"></div>
+              <div class="d-table-row">
+                <div class="d-table-cell size text-right">
+                  XXXL
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+                <div class="d-table-cell text-center">
+                  <div>78-82</div>
+                  <div>62-66</div>
+                  <div>96-90</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="content clearfix">
+          <p>Dessa mått anger ungefärlig storleksguide, mät dig enligt våra mätanvisningar så får du "passa in" dina mått och hitta storlek som passar dig.</p>
+          <p>När du mäter dina mått för att köpa en kjol så är STUSSVIDD-måttet det viktigaste. Midjan kommer
+          kännas lite stor på vissa men "det är alltid lättare att ta in än att lägga ut"!</p>
+        </div>
+    </div>
+  </section>
+
+  @include('sections.section-thankyou')
+  @include('sections.section-newsletter')
+  @include('sections.section-three-promo')
+
+@endsection
