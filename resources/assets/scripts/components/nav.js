34a23e86c630e3acb5eeedaffeeb246ebba489ce
Marc Anniel Vino <marc.vino@stokmanila.com>
2019-06-17 19:18:00 +0800

    Navbar Collapses JS

--- resources/assets/scripts/components/nav.js
+++ resources/assets/scripts/components/nav.js
@@ -29,10 +29,15 @@ Nav.menuPosition = function() {
   window.onresize = function () {
     headerPosition = header.position().top + header.outerHeight(true);
     changePosition(menu, headerPosition);
+    console.log('onresize');
   };
 
   function changePosition(menu, headerPosition) {
     menu.css('margin-top', headerPosition + 'px');
+    console.log(headerPosition);
+
+    console.log('header position top: ' + header.position().top);
+    console.log('outerheight: ' + header.outerHeight(true));
   }
 };
 
