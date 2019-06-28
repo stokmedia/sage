d9375ef617af1285735608b4a8d558fbaf145a95
Mark Anthony Bautista <mark.bautista@stokmanila.com>
2019-06-28 13:26:26 +0800

    add srcset to product images

--- resources/views/partials/product-images.blade.php
+++ resources/views/partials/product-images.blade.php
@@ -3,6 +3,6 @@
 	    @if ($loop->index >= $limit)
 	        @break
 	    @endif
-		<img class="{{ ($loop->index+1 === $img_pos_to_add_class) ? $class.' lazy' : 'lazy' }}" data-src="{{ $image['url'] }}" />
+		<img class="{{ ($loop->index+1 === $img_pos_to_add_class) ? $class.' lazy' : 'lazy' }}" data-src="{{ $image['url'] }}" data-srcset="{{ $image['standard']['url'] }} 720w, {{ $image['standard']['url'] }} 1440w, {{ $image['big']['url'] }} 2880w"/>
 	@endforeach
 @endif
