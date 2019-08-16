b4db20f5f040163c257abf5f58dd187d4631a154
vannymagalong <vannessa@stokmanila.com>
2019-08-16 13:53:16 +0800

    Category banner and thank you receipt update

--- resources/views/sections/section-category-banner.blade.php
+++ resources/views/sections/section-category-banner.blade.php
@@ -1,8 +1,10 @@
 @if ($hero_banner->image || $hero_banner->image_mobile)
-    <section class="section category-banner d-flex no-mb js-image-switch"
+    {{-- <section class="section category-banner d-flex no-mb js-image-switch"
         style="background-image:url( {{ $hero_banner->image }} );"
         data-desktop-background="{{ $hero_banner->image }}"
-        data-mobile-background="{{ $hero_banner->image_mobile }}">
+        data-mobile-background="{{ $hero_banner->image_mobile }}"> --}}
+    <section class="section category-banner d-flex no-mb lazy"
+        data-bg="url( {{ wp_is_mobile() ? $hero_banner->image_mobile : $hero_banner->image }} )">  
         <div class="box-wrapper d-flex align-self-end">
             <div class="container">
                 <div class="category-banner-info text-center text-sm-left">
--- resources/views/sections/section-thankyou.blade.php
+++ resources/views/sections/section-thankyou.blade.php
@@ -18,37 +18,37 @@
                 @if (isset($receipt_info['order']))
                     <div class="summary">
                         @if (!empty($page_info->summary_label))
-                        <h3 class="h3">{{ $page_info->summary_label ?? '' }}</h3>
+                        <h3 class="h3">{!! $page_info->summary_label ?? '' !!}</h3>
                         @endif
 
                         <div class="summary-group">
                             <div class="summary-item">
-                                <div class="title">@php(!empty( $translation['sub_total'] ) ? $translation['sub_total'] : null)</div>
-                                <div class="price">@php($receipt_info['totals']['itemsTotalPrice'])</div>
+                                <div class="title">{!! !empty( $translation['sub_total'] ) ? $translation['sub_total'] : null !!}</div>
+                                <div class="price">{!! $receipt_info['totals']['itemsTotalPrice'] !!}</div>
                             </div>
 
                             @if( !empty($receipt_info['totals']['totalDiscountPriceAsNumber']) )
                             <div class="summary-item">
-                                <div class="title">@php(!empty( $translation['discount'] ) ? $translation['discount'] : null)</div>
-                                <div class="price">@php($receipt_info['totals']['totalDiscountPrice'])</div>
+                                <div class="title">{!! !empty( $translation['discount'] ) ? $translation['discount'] : null !!}</div>
+                                <div class="price">{!! $receipt_info['totals']['totalDiscountPrice'] !!}</div>
                             </div>
                             @endif
 
                             @if( !empty($receipt_info['totals']['taxDeductedAsNumber']) )
                             <div class="summary-item">
-                                <div class="title">@php(!empty( $translation['vat_included'] ) ? $translation['vat_included'] : null)</div>
-                                <div class="price">@php($receipt_info['totals']['taxDeductedAsNumber'])</div>
+                                <div class="title">{!! !empty( $translation['vat_included'] ) ? $translation['vat_included'] : null !!}</div>
+                                <div class="price">{!! $receipt_info['totals']['taxDeductedAsNumber'] !!}</div>
                             </div>
                             @endif
                             
                             <div class="summary-item">
-                                <div class="title">@php(!empty( $translation['shipping'] ) ? $translation['shipping'] : null)</div>
-                                <div class="price">@php($receipt_info['totals']['shippingPrice'])</div>
+                                <div class="title">{!! !empty( $translation['shipping'] ) ? $translation['shipping'] : null !!}</div>
+                                <div class="price">{!! $receipt_info['totals']['shippingPrice'] !!}</div>
                             </div>
 
                             <div class="summary-item">
-                                <div class="title">@php(!empty( $translation['total'] ) ? $translation['total'] : null)</div>
-                                <div class="price"><strong>@php($receipt_info['totals']['grandTotalPrice'])</strong></div>
+                                <div class="title">{!! !empty( $translation['total'] ) ? $translation['total'] : null !!}</div>
+                                <div class="price"><strong>{!! $receipt_info['totals']['grandTotalPrice'] !!}</strong></div>
                             </div>
                         </div>
                     </div>
@@ -60,5 +60,13 @@
 
             </div>
         </div>
+
+        {{-- Klarna Snippet --}}
+        @if (!empty($receipt_info['paymentMethodData']['snippet']))
+            <div class="row">
+                {!! $receipt_info['paymentMethodData']['snippet'] !!}
+            </div>
+        @endif            
+
     </div>
 </section>
