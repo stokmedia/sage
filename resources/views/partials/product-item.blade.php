639698e871c41eab1894d376b96e3b2d6370b2f4
Louie Malafo <louie@stokmanila.com>
2019-09-02 23:01:05 +0800

    Fix product link

--- resources/views/partials/product-item.blade.php
+++ resources/views/partials/product-item.blade.php
@@ -1,11 +1,11 @@
-{{-- <h2><a href="{{ get_the_permalink() }}">{{ $post->post_title }}</a></h2>
+{{-- <h2><a href="{{ get_the_permalink( $post ) }}">{{ $post->post_title }}</a></h2>
 
 <div>
 	@include('partials.product-price', ['priceInfo' => $product->price] )
 </div>
 
 <div>
-	<a href="{{ get_the_permalink() }}">@include('partials.product-images', ['images' => $product->images, 'size' => 'thumb', 'limit' => 2] )</a>
+	<a href="{{ get_the_permalink( $post ) }}">@include('partials.product-images', ['images' => $product->images, 'size' => 'thumb', 'limit' => 2] )</a>
 </div> --}}
 
 @php (
