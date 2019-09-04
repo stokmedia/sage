3e9fff1bf819f4338ced64208be8eaf8ea21ddee
vannymagalong <vannessa@stokmanila.com>
2019-09-04 13:48:43 +0800

    Order summary update

--- resources/parts/shop/totals-selection.php
+++ resources/parts/shop/totals-selection.php
@@ -4,10 +4,13 @@
 
     // Translations
     $translations = App::getSiteTranslations()->selections;
-    $totalLabel = !empty($translations['total_price']) ? $translations['total_price'] : '';
-    $vatLabel = !empty($translations['vat']) ? $translations['vat'] : '';
-    $shippingLabel = !empty($translations['shipping']) ? $translations['shipping'] : '';
-    $subTotalLabel = !empty($translations['sub_total']) ? $translations['sub_total'] : '';
+    $totalLabel = !empty($translations['total_price']) ? $translations['total_price'] : 'Total';
+    $discountLabel = !empty($translations['discount']) ? $translations['discount'] : 'Discount';
+    $shippingLabel = !empty($translations['shipping']) ? $translations['shipping'] : 'Shipping';
+    $handlingCostLabel = !empty($translations['handling_cost']) ? $translations['handling_cost'] : 'Handling Cost';
+    $vatLabel = !empty($translations['vat']) ? $translations['vat'] : 'VAT';    
+    $vatIncludedLabel = !empty($translations['vat_included']) ? $translations['vat_included'] : 'VAT included';
+    $subTotalLabel = !empty($translations['sub_total']) ? $translations['sub_total'] : 'Sub total';
 
     // Classes
     $rowClass = !empty($rowClass) ? $rowClass : '';
@@ -24,19 +27,40 @@
             </div>
         <?php endif ;?>
 
-        <?php if (!empty($selection['totals']['taxDeductedAsNumber'])) : ?>
+        <?php if (!empty($selection['totals']['totalDiscountPrice'])) : ?>
             <div class="summary-item <?php echo $rowClass ?>">
-                <div class="title <?php echo $labelClass ?>"><?php echo $vatLabel ?></div>
-                <div class="price <?php echo $valueClass ?>"><?php echo $selection['totals']['taxDeducted']; ?></div>
+                <div class="title <?php echo $labelClass ?>"><?php echo $discountLabel ?></div>
+                <div class="price <?php echo $valueClass ?>"><?php echo $selection['totals']['totalDiscountPrice']; ?></div>
             </div>
-        <?php endif ;?>
+        <?php endif ;?>    
 
-        <?php if (!empty($selection['totals']['shippingPrice'])) : ?>
+        <?php if (!empty($selection['totals']['shippingPriceAsNumber'])) : ?>
             <div class="summary-item <?php echo $rowClass ?>">
                 <div class="title <?php echo $labelClass ?>"><?php echo $shippingLabel ?></div>
                 <div class="price <?php echo $valueClass ?>"><?php echo $selection['totals']['shippingPrice']; ?></div>
             </div>
         <?php endif ;?>
+
+        <?php if (!empty($selection['totals']['handlingCostPriceAsNumber'])) : ?>
+            <div class="summary-item <?php echo $rowClass ?>">
+                <div class="title <?php echo $labelClass ?>"><?php echo $handlingCostLabel ?></div>
+                <div class="price <?php echo $valueClass ?>"><?php echo $selection['totals']['handlingCostPrice']; ?></div>
+            </div>
+        <?php endif ;?>  
+        
+        <?php if (!empty($selection['totals']['taxDeductedAsNumber'])) : ?>
+            <div class="summary-item <?php echo $rowClass ?>">
+                <div class="title <?php echo $labelClass ?>"><?php echo $vatLabel ?></div>
+                <div class="price <?php echo $valueClass ?>"><?php echo $selection['totals']['taxDeducted']; ?></div>
+            </div>
+        <?php endif ;?>        
+
+        <?php if (!empty($selection['totals']['grandTotalPriceTaxAsNumber'])) : ?>
+            <div class="summary-item <?php echo $rowClass ?>">
+                <div class="title <?php echo $labelClass ?>"><?php echo $vatIncludedLabel. ' ('. $selection['totals']['taxPercent'] .'%)'; ?></div>
+                <div class="price <?php echo $valueClass ?>"><?php echo $selection['totals']['grandTotalPriceTax']; ?></div>
+            </div>
+        <?php endif ;?>        
 
         <?php if (!empty($selection['totals']['grandTotalPrice'])) : ?>
             <div class="summary-item <?php echo $rowClass ?>">
