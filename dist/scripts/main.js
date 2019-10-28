3a745425c1754fbfe664eed10ec7efbf79b3f296
Marc Anniel Vino <marc.vino@stokmanila.com>
2019-10-28 14:36:43 +0800

    IOS 13.1.3 Flickity Fix Swipe

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -16652,6 +16652,39 @@ var sliders = {
   init: function init() {
     __webpack_require__(35);
     __webpack_require__(36);
+
+    // IOS 13.1.3 Flickity Fix
+    var touchingCarousel = false;
+    var touchStartCoords;
+
+    document.body.addEventListener('touchstart', function(e) {
+      if (e.target.closest('.flickity-slider')) {
+        touchingCarousel = true;
+      } else {
+        touchingCarousel = false;
+        return;
+      }
+
+      touchStartCoords = {
+        x: e.touches[0].pageX,
+        y: e.touches[0].pageY,
+      }
+    });
+
+    document.body.addEventListener('touchmove', function(e) {
+      if (!(touchingCarousel && e.cancelable)) {
+        return;
+      }
+
+      var moveVector = {
+        x: e.touches[0].pageX - touchStartCoords.x,
+        y: e.touches[0].pageY - touchStartCoords.y,
+      };
+
+      if (Math.abs(moveVector.x) > 7)
+        { e.preventDefault() }
+
+    }, {passive: false});
   },
 }
 
--- resources/assets/scripts/components/sliders/index.js
+++ resources/assets/scripts/components/sliders/index.js
@@ -2,5 +2,38 @@ export const sliders = {
   init() {
     require('./product-selected');
     require('./popular-products');
+
+    // IOS 13.1.3 Flickity Fix
+    let touchingCarousel = false;
+    let touchStartCoords;
+
+    document.body.addEventListener('touchstart', function(e) {
+      if (e.target.closest('.flickity-slider')) {
+        touchingCarousel = true;
+      } else {
+        touchingCarousel = false;
+        return;
+      }
+
+      touchStartCoords = {
+        x: e.touches[0].pageX,
+        y: e.touches[0].pageY,
+      }
+    });
+
+    document.body.addEventListener('touchmove', function(e) {
+      if (!(touchingCarousel && e.cancelable)) {
+        return;
+      }
+
+      var moveVector = {
+        x: e.touches[0].pageX - touchStartCoords.x,
+        y: e.touches[0].pageY - touchStartCoords.y,
+      };
+
+      if (Math.abs(moveVector.x) > 7)
+        e.preventDefault()
+
+    }, {passive: false});
   },
 }
