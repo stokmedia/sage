09c2554b76c37fc50a6572e6f16c5d6d73304f4e
Dave Manuel <dave.manuel@stok.se>
2020-12-03 15:00:53 +0800

    Add css for pre-order tag

--- app/setup.php
+++ app/setup.php
@@ -11,8 +11,8 @@ use Roots\Sage\Template\BladeProvider;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], 1583758214, true);
+    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, 1606978809);
+    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], 1606978809, true);
 
     if (is_single() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
